0a2043b60e432fa33de3c9cfc12f0b25e2d1c71e
DB913 <dimabradu913@gmail.com>
2023-08-10 18:12:12 +0300

    add test for teacher

--- DemoUniversityTests/Unit/TeacherTest.cs
+++ DemoUniversityTests/Unit/TeacherTest.cs
@@ -1,32 +1,50 @@
+using Bogus;
+using DemoUniversity.Domain.Models;
+
 namespace DemoUniversityTests.Unit;
 
 public class TeacherTest
 {
-    // [Fact]
-    // public void SetCreateStudentPositiveTest()
-    // {
-    //     var studentId = Guid.NewGuid();
-    //     var faker = new Faker("ru");
-    //     var lastName = faker.Name.LastName();
-    //     var firstName = faker.Name.FirstName();
-    //     var middleName = faker.Name.FirstName();
-    //     var address = "street Cucueva 20/3. Part 10";
-    //     faker.Address.FullAddress();
-    //     var phone = "+37377941321";
-    //     var age = 25;
-    //     var speciality = "doctor";
-    //     var course = 4;
-    //
-    //     var student = new Student(studentId, lastName, firstName, middleName, address, phone, age, speciality, course);
-    //
-    //     Assert.True(student.Id == studentId);
-    //     Assert.True(student.LastName == lastName);
-    //     Assert.True(student.FirstName == firstName);
-    //     Assert.True(student.MiddleName == middleName);
-    //     Assert.True(student.Address == address);
-    //     Assert.True(student.Phone == phone);
-    //     Assert.True(student.Age == age);
-    //     Assert.True(student.Speciality == speciality);
-    //     Assert.True(student.Course == course);
-    // }
+    [Fact]
+    public void SetCreateTeacherPositiveTest()
+    {
+        var teacherId = Guid.NewGuid();
+        var faker = new Faker("ru");
+        var lastName = faker.Name.LastName();
+        var firstName = faker.Name.FirstName();
+        var middleName = faker.Name.FirstName();
+        var address = faker.Address.FullAddress();
+        var phone = "+37377941321";
+        var age = 25;
+
+        var departmentId = Guid.NewGuid();
+        var departmentName = faker.Company.CompanyName()+faker.Company.CompanyName();
+        const string departmentShortName = "test";
+        var department = new Department(departmentId, departmentName, departmentShortName);
+
+        const int disciplineId = 1;
+        var disciplineName = faker.Name.LastName();
+        var discipline = new Discipline(disciplineId, disciplineName);
+
+        var teacher = new Teacher(teacherId,
+            lastName,
+            firstName,
+            middleName,
+            address,
+            phone,
+            age,
+            department,
+            discipline
+        );
+
+        Assert.True(teacher.Id == teacherId);
+        Assert.True(teacher.LastName == lastName);
+        Assert.True(teacher.FirstName == firstName);
+        Assert.True(teacher.MiddleName == middleName);
+        Assert.True(teacher.Department.Id == departmentId);
+        Assert.True(teacher.Department.Name == departmentName);
+        Assert.True(teacher.Department.ShortName == departmentShortName);
+        Assert.True(teacher.Discipline.Name == disciplineName);
+        Assert.True(teacher.Discipline.Id == disciplineId);
+    }
 }
